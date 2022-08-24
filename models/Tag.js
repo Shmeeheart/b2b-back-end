7cad46ea0c7373a2d1045baa02aac436c090993d
Jennifer <goodwije@gmail.com>
2022-08-23 20:53:49 -0600

    added product delete

--- models/Tag.js
+++ models/Tag.js
@@ -7,7 +7,7 @@ class Tag extends Model {}
 Tag.init(
   {
     id: {
-      type: DatayTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
